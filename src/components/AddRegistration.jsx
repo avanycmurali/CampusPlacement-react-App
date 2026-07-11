1561612f62ff70aa01c523410bfecb8c77b7e99e
avanycmurali <avanycmuralidharan@gmail.com>
2026-07-11 11:36:49 +0530

    Student registration page added

---
+++ src/components/AddRegistration.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+const AddRegistration = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-4">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Registration Number</label>
+<input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Full name</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Aadhar Number</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Email Address</label>
+<input type="email" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">Phone Number</label>
+<input type="tel" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+<label htmlFor="" className="form-label">Branch</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+
+<label htmlFor="" className="form-label">SSLC Mark %</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+<label htmlFor="" className="form-label">Plus Two Mark %</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">UG Degree Name and Mark</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<label htmlFor="" className="form-label">PG Degree Name and Mark</label>
+<input type="text" className="form-control" />
+
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-success">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default AddRegistration
